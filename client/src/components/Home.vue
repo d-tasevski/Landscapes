6f337c46cd0864943bd0124d10607d44a9d27363
Darkø Tasevski <darko.tasevski@redbox.com>
2018-11-14 20:53:30 +0100

    feat: Vue Apollo config

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,14 +1,35 @@
 <template>
 	<v-container>
 		<div><h1>Home</h1></div>
-		<v-btn color="primary">Button</v-btn>
+		<ul v-for="post in getPosts" :key="post._id">
+			<li>{{ post.title }} {{ post.imageUrl }} {{ post.description }}</li>
+			<li>{{ post.likes }}</li>
+		</ul>
 	</v-container>
 </template>
 
 <script>
 // @ is an alias to /src
+import { gql } from 'apollo-boost';
 
 export default {
 	name: 'home',
+	apollo: {
+		getPosts: {
+			query: gql`
+				query {
+					getPosts {
+						_id
+						title
+						categories
+						createdBy {
+							username
+							email
+						}
+					}
+				}
+			`,
+		},
+	},
 };
 </script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,24 @@
 import '@babel/polyfill';
 import Vue from 'vue';
+import ApolloClient from 'apollo-boost';
+import VueApollo from 'vue-apollo';
+
 import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-Vue.config.productionTip = false;
+Vue.use(VueApollo);
+
+const defaultClient = new ApolloClient({
+	uri: 'http://localhost:4000/graphql',
+});
+
+const apolloProvider = new VueApollo({ defaultClient });
 
 new Vue({
+	// Inject apollo to all components so that we can perform queries and mutations
+	provider: apolloProvider.provide(),
 	router,
 	store,
 	render: h => h(App),
