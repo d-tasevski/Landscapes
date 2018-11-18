d8a3c88405ea4f5c7c0e7a72cca865f5be82ddfd
Darkø Tasevski <darko.tasevski@redbox.com>
2018-11-18 18:43:08 +0100

    feat: Smart queries

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,10 +1,24 @@
 <template>
 	<v-container>
 		<div><h1>Home</h1></div>
-		<ul v-for="post in getPosts" :key="post._id">
-			<li>{{ post.title }} {{ post.imageUrl }} {{ post.description }}</li>
-			<li>{{ post.likes }}</li>
-		</ul>
+		<ApolloQuery :query="getPostsQuery">
+			<template
+				slot-scope="{
+					result: {
+						loading,
+						error,
+						data: { getPosts },
+					},
+				}"
+			>
+				<div v-if="loading">Loading...</div>
+				<div v-else-if="error">{{ error.message }}</div>
+				<ul v-else v-for="post in getPosts" :key="post._id">
+					<li>{{ post.title }} {{ post.imageUrl }} {{ post.description }}</li>
+					<li>{{ post.likes }}</li>
+				</ul>
+			</template>
+		</ApolloQuery>
 	</v-container>
 </template>
 
@@ -14,14 +28,17 @@ import { gql } from 'apollo-boost';
 
 export default {
 	name: 'home',
-	apollo: {
-		getPosts: {
-			query: gql`
+	data() {
+		return {
+			getPostsQuery: gql`
 				query {
 					getPosts {
 						_id
 						title
 						categories
+						imageUrl
+						description
+						likes
 						createdBy {
 							username
 							email
@@ -29,7 +46,15 @@ export default {
 					}
 				}
 			`,
-		},
+			result({ data, loading }) {
+				if (!loading) {
+					this.posts = data.getPosts;
+				}
+			},
+			error(err) {
+				throw new Error(err);
+			},
+		};
 	},
 };
 </script>
